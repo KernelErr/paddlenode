df6163cff365a81e1f393bae9d6fd1edc79cdf48
Kevin Li <45716019+KernelErr@users.noreply.github.com>
2020-07-24 19:10:23 +0800

    [Base] Support image classification.
    Using OpenCV to process image, and infer its class.

--- binding.gyp
+++ binding.gyp
@@ -1,22 +1,29 @@
 {
     'variables': {
-        'lite_dir%': '', # Path of Paddle Lite prebuild libraries for Windows x86
+        'lite_dir%': '', #C:/inference_lite_lib.win.x86.MSVC.C++_static.py37.full_publish
+        'opencv_dir%': '' #C:/opencv/build
     },
     "targets": [
         {
             'target_name': "paddlenode",
-            'sources': ["nodehelper.h","paddlelib.h","paddlelib.cc","paddlenode.cc"],
-            'defines': [
+            'sources': [
+                "./src/nodehelper.h",
+                "./src/paddlelib.cc",
+                "./src/paddlelib.h",
+                "./src/paddlenode.cc",
             ],
             'include_dirs': [
                 "<(lite_dir)/cxx/include",
-                "<(lite_dir)/third_party/mklml/include"
+                "<(lite_dir)/third_party/mklml/include",
+                "<(opencv_dir)/include"
             ],
             'libraries': [
                 "-l<(lite_dir)/cxx/lib/libpaddle_api_light_bundled.lib",
                 "-l<(lite_dir)/third_party/mklml/lib/libiomp5md.lib",
                 "-l<(lite_dir)/third_party/mklml/lib/mklml.lib",
-                "-lshlwapi.lib"
+                "-lshlwapi.lib",
+                "-l<(opencv_dir)/x64/vc15/lib/opencv_world440.lib",
+                "-l<(opencv_dir)/x64/vc15/lib/opencv_world440d.lib"
             ]
         }
     ]
--- src/paddlenode.cc
+++ src/paddlenode.cc
@@ -3,11 +3,16 @@
 #include <vector>
 #include <node_api.h>
 #include <assert.h>
+#include <opencv2/opencv.hpp>
 #include "paddlelib.h"
 #include "nodehelper.h"
 using namespace paddle::lite_api;
+using namespace cv;
 
 PaddleLite paddlelite;
+const std::vector<int64_t> INPUT_SHAPE = {1, 3, 224, 224};
+const std::vector<float> INPUT_MEAN = {0.485f, 0.456f, 0.406f};
+const std::vector<float> INPUT_STD = {0.229f, 0.224f, 0.225f};
 
 napi_value set_model_file(napi_env env, napi_callback_info info)
 {
@@ -130,13 +135,105 @@ napi_value infer_float(napi_env env, napi_callback_info info)
     infer_res = paddlelite.infer_float(input_data, shape);
     napi_value ret;
     NAPI_CALL(env, napi_create_array(env, &ret));
-    for (i = 0; i < *infer_res; i++)
+    for (i = 0; i < *infer_res - 1; i++)
     {
         napi_value e;
-        NAPI_CALL(env, napi_create_double(env, *(infer_res + i), &e));
+        NAPI_CALL(env, napi_create_double(env, *(infer_res + i + 1), &e));
         NAPI_CALL(env, napi_set_element(env, ret, i, e));
     }
     return ret;
+}
+
+napi_value image_file_classfication(napi_env env, napi_callback_info info)
+{
+    char *image_path = new char[1001];
+    double scalefactor;
+    int size[2], mean[3];
+    uint32_t size_length, mean_length, shape_length, i;
+    bool swapRB;
+    size_t argc = 6; // Image Path, shape, scalefactor, size, mean, swapRB
+    napi_value args[6];
+    NAPI_CALL(env, napi_get_cb_info(env, info, &argc, args, nullptr, nullptr));
+    if (argc != 6)
+    {
+        napi_throw_error(env, NULL, "Check the amount of arguments.");
+        return NULL;
+    }
+    napi_valuetype path_valuetype, shape_valuetype, scalefactor_valuetype, size_valuetype, mean_valuetype, swapRB_valuetype;
+    NAPI_CALL(env, napi_typeof(env, args[0], &path_valuetype));
+    NAPI_CALL(env, napi_typeof(env, args[1], &shape_valuetype));
+    NAPI_CALL(env, napi_typeof(env, args[2], &scalefactor_valuetype));
+    NAPI_CALL(env, napi_typeof(env, args[3], &size_valuetype));
+    NAPI_CALL(env, napi_typeof(env, args[4], &mean_valuetype));
+    NAPI_CALL(env, napi_typeof(env, args[5], &swapRB_valuetype));
+    if (path_valuetype != napi_string)
+    {
+        napi_throw_error(env, NULL, "Wrong argument type.1");
+        return NULL;
+    }
+    if (path_valuetype != napi_string || shape_valuetype != napi_object || scalefactor_valuetype != napi_number || size_valuetype != napi_object || mean_valuetype != napi_object || swapRB_valuetype != napi_boolean)
+    {
+        napi_throw_error(env, NULL, "Wrong argument type.");
+        return NULL;
+    }
+    NAPI_CALL(env, napi_get_value_string_utf8(env, args[0], image_path, sizeof(char) * 1001, NULL));
+    try
+    {
+        Mat input_image = imread(image_path);
+        if (input_image.empty())
+        {
+            napi_throw_error(env, NULL, "Unable to read image.");
+            return NULL;
+        }
+        NAPI_CALL(env, napi_get_value_double(env, args[2], &scalefactor));
+        NAPI_CALL(env, napi_get_array_length(env, args[3], &size_length));
+        NAPI_CALL(env, napi_get_array_length(env, args[4], &mean_length));
+        if (size_length != 2 || mean_length != 3)
+        {
+            napi_throw_error(env, NULL, "Wrong array length.");
+            return NULL;
+        }
+        for (i = 0; i < 2; i++)
+        {
+            napi_value e;
+            NAPI_CALL(env, napi_get_element(env, args[3], i, &e));
+            NAPI_CALL(env, napi_get_value_int32(env, e, &size[i]));
+        }
+        for (i = 0; i < 3; i++)
+        {
+            napi_value e;
+            NAPI_CALL(env, napi_get_element(env, args[4], i, &e));
+            NAPI_CALL(env, napi_get_value_int32(env, e, &mean[i]));
+        }
+        NAPI_CALL(env, napi_get_value_bool(env, args[5], &swapRB));
+        Mat inputBlob = dnn::blobFromImage(input_image, scalefactor, Size(size[0], size[1]), Scalar(mean[0], mean[1], mean[2]), swapRB);
+        std::vector<int64_t> shape;
+        NAPI_CALL(env, napi_get_array_length(env, args[1], &shape_length));
+        for (i = 0; i < shape_length; i++)
+        {
+            napi_value e;
+            int g;
+            NAPI_CALL(env, napi_get_element(env, args[1], i, &e));
+            NAPI_CALL(env, napi_get_value_int32(env, e, &g));
+            shape.push_back(g);
+        }
+        float *infer_res;
+        infer_res = paddlelite.infer_float((float *)inputBlob.data, shape);
+        napi_value ret;
+        NAPI_CALL(env, napi_create_array(env, &ret));
+        for (i = 0; i < *infer_res - 1; i++)
+        {
+            napi_value e;
+            NAPI_CALL(env, napi_create_double(env, *(infer_res + i + 1), &e));
+            NAPI_CALL(env, napi_set_element(env, ret, i, e));
+        }
+        return ret;
+    }
+    catch (const std::exception &e)
+    {
+        napi_throw_error(env, NULL, e.what());
+        return NULL;
+    }
 }
 
 #define DECLARE_NAPI_METHOD(name, func)         \
@@ -151,9 +248,9 @@ napi_value Init(napi_env env, napi_value exports)
         DECLARE_NAPI_METHOD("set_model_file", set_model_file),
         DECLARE_NAPI_METHOD("set_threads", set_threads),
         DECLARE_NAPI_METHOD("set_power_mode", set_power_mode),
-        DECLARE_NAPI_METHOD("infer_float", infer_float)
-    };
-    status = napi_define_properties(env, exports, 4, descriptors);
+        DECLARE_NAPI_METHOD("infer_float", infer_float),
+        DECLARE_NAPI_METHOD("image_file_classfication", image_file_classfication)};
+    status = napi_define_properties(env, exports, 5, descriptors);
     assert(status == napi_ok);
     return exports;
 }
